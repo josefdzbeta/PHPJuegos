4e1b77ff45035779c46017ed2dca19b031294693
josefdzbeta <jafb19@outlook.com>
2021-12-17 12:22:39 +0100

    config

---
+++ config/configdb.php
@@ -0,0 +1,13 @@
+<?php
+    define('DB_SERVER','localhost');
+    define('DB_USERNAME','root');
+    define('DB_PASSWORD','');
+    define('DB_NAME','Juegos');
+
+    //Nueva conexión mysqli 
+    $mysqli = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+
+    if($mysqli === false){
+        die('Error: no te has podido conectar'.mysqli->connect_error);
+    }
+?>
